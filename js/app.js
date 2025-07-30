02eb70a81a8ce426a19e30d8e7983a6bed3e0f7c
Ashutosh <ashuma2721@gmail.com>
2025-07-30 14:27:07 +0530

    added language selection

--- js/app.js
+++ js/app.js
@@ -1,21 +1,38 @@
 import {createWorker, RecognizeResult} from 'tesseract.js';
 import { jsPDF } from "jspdf";
 import { addImageToPage, embedPdf} from "./utils";
+import {languages} from "./languages";
 
 const uploader = document.getElementById('file-upload');
 const fileList = document.getElementById('file-list');
+const pdfContainer = document.getElementById('pdf-container');
 
-const doc = new jsPDF();
-const worker = await createWorker('eng');
-const filePromises = [];
+let language = localStorage.getItem("language") ?? "eng";
 
+const languageSelect = document.getElementById("language-select");
+languages.forEach((lang) => {
+  const option = document.createElement("option");
+  option.appendChild(document.createTextNode(lang.name));
+  option.value = lang.code;
+  languageSelect.appendChild(option);
+});
 
-
+languageSelect.value = language;
+languageSelect.addEventListener("change", (event) => {
+  language = event.target.value;
+  localStorage.setItem("language", language);
+});
 
 
 uploader.addEventListener('change', async (event) => {
   const files = event.target.files;
+
+  let doc = new jsPDF();
+  let filePromises = [];
+  const worker = await createWorker(language);
+
   fileList.innerHTML = '';
+  pdfContainer.innerHTML = '';
 
   Array.from(files).forEach((file) => {
     const fileItem = addImageToPage(file);
@@ -23,10 +40,10 @@ uploader.addEventListener('change', async (event) => {
     filePromises.push(worker.recognize(file))
   });
 
-  await generatePDF()
+  await generatePDF(doc, filePromises)
   // Get PDF as data URL
   const pdfDataUrl = doc.output('datauristring');
-  embedPdf(pdfDataUrl, document.getElementById('pdf-container'));
+  embedPdf(pdfDataUrl, pdfContainer);
 });
 
 
@@ -34,11 +51,12 @@ uploader.addEventListener('change', async (event) => {
 
 
 
-async function generatePDF() {
-  const recognizeResults = await Promise.allSettled(filePromises);
+async function generatePDF(doc, workerFiles) {
+  const recognizeResults = await Promise.allSettled(workerFiles);
 
   recognizeResults.forEach((res) => {
     const text = res.value.data.text
+    console.log('text', text)
     doc.text(text, 10, 10);
     if (doc.getNumberOfPages() < recognizeResults.length) doc.addPage();
   })
